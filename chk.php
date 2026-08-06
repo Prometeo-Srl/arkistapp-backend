5485d245583102396543378ba6d2fde9fe102376
simoruv <simone.ruvolo@algomera.it>
2026-08-06 15:25:57 +0200

    pint code style

--- chk.php
+++ chk.php
@@ -1,4 +1,7 @@
 <?php
-echo "ok-never exists: ".\App\Models\User::where('email','ok-never@example.com')->count()."\n";
-echo "a@x.com exists: ".\App\Models\User::where('email','a@x.com')->count()."\n";
-echo "marta-bulk exists: ".\App\Models\User::where('email','marta-bulk@example.com')->count()."\n";
+
+use App\Models\User;
+
+echo 'ok-never exists: '.User::where('email', 'ok-never@example.com')->count()."\n";
+echo 'a@x.com exists: '.User::where('email', 'a@x.com')->count()."\n";
+echo 'marta-bulk exists: '.User::where('email', 'marta-bulk@example.com')->count()."\n";
